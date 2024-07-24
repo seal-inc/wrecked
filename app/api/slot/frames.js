46d2b6ee4c302c6cce4113ff9a0245d5fd8aa8ae
robo360 <berwa05@gmail.com>
2024-07-24 14:26:13 -0400

    add validation

--- app/api/slot/frames.js
+++ app/api/slot/frames.js
@@ -1,5 +1,16 @@
 import { createFrames } from "frames.js/next";
+import { farcasterHubContext, neynarValidate } from "frames.js/middleware";
 
 export const frames = createFrames({
   basePath: "/api/slot",
+  middleware: [
+    farcasterHubContext({
+      hubHttpUrl: "https://hub-api.neynar.com",
+      hubRequestOptions: {
+        headers: {
+          "x-api-key": "C32483E3-C3DF-44C6-BBA9-F39F537C23F2",
+        },
+      },
+    }),
+  ],
 });
